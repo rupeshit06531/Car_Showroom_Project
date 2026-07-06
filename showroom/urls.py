01af5a6ab38e0e3a8129cda8896ace67944f58a5
Rupesh Kumar Mahto <rupeshit06531@gmail.com>
2026-07-06 16:10:53 +0530

    Added base template and updated home page

--- showroom/urls.py
+++ showroom/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add-car/', views.add_car, name='add_car'),
+     path("edit-car/<int:id>/", views.edit_car, name="edit_car"),
 ]
--- showroom/views.py
+++ showroom/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car
+from .forms import CarForm
 
 def home(request):
     cars = Car.objects.all()
     return render(request, 'showroom/home.html', {'cars': cars})
+
+def add_car(request):
+    if request.method == "POST":
+        form = CarForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = CarForm()
+
+    return render(request, 'showroom/add_car.html', {'form': form})
+
+from django.shortcuts import get_object_or_404
+
+def edit_car(request, id):
+    car = get_object_or_404(Car, id=id)
+
+    if request.method == "POST":
+        form = CarForm(request.POST, instance=car)
+
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    else:
+        form = CarForm(instance=car)
+
+    return render(request, "showroom/add_car.html", {
+        "form": form
+    })
